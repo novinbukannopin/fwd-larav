506b7d1a8f0d330e66c37d271c5ad35b6e103863
novinbukannopin <68802465+novinbukannopin@users.noreply.github.com>
2023-02-15 20:17:37 +0700

    add foreign keys user

---
+++ database/migrations/2023_02_15_131102_add_foreign_keys_to_detail_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            $table->foreign('user_id', 'fk_detail_user_to_users')->reference('id')->on('users')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('type_user_id', 'fk_detail_user_to_type_user')->reference('id')->on('type_user')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            $table->dropForeign('fk_detail_user_to_users');
+            $table->dropForeign('fk_detail_user_to_type_user');
+        });
+    }
+};
